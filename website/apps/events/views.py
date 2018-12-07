4495e1f4a34ba6decc428ebda906176ce49d117c
PatrickKvartsaniy <kvartsaniy@gmail.com>
2018-12-07 20:07:56 +0200

    refactoring

--- website/apps/events/views.py
+++ website/apps/events/views.py
@@ -7,9 +7,9 @@ def index(request):
     return render(request,"events/events.html",{"events":events})
 
 def international(request):
-    events = Event.objects.all().filter(type="international")
+    events = Event.objects.all().filter(event_type="international")
     return render(request,"events/events.html",{"events":events,"title":"International"})
 
 def local(request):
-    events = Event.objects.all().filter(type="local")
+    events = Event.objects.all().filter(event_type="local")
     return render(request,"events/events.html",{"events":events,"title":"Local"})
